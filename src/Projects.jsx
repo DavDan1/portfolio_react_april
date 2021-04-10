9f17b5028c652679c51e430f9a0aa312d2fe6573
DavDan1 <david-danielyan@hotmail.com>
2021-04-10 14:05:23 +0200

    added packaje.json to data folderand modified it with id and name

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>
-        React Router has, among others, the Switch and Route components. The
-        Route component renders a page if the current URL location matches the
-        path prop specified in it.
-      </p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "My First Website",
+      },
+      {
+        id: 2,
+        name: "FizzBuzz",
+      },
+    ],
+  };
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
 
 export default Projects;
