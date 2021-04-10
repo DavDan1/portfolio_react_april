4a7a82f88db179f3351423315bb51387cc6f1935
DavDan1 <david-danielyan@hotmail.com>
2021-04-10 15:39:19 +0200

    bugfixed, had issue in ..projects.feature.js file, some words were misplaced

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -7,28 +7,29 @@ describe("User can see list of projects", () => {
   it("displays the first project", () => {
     cy.get("#project-1").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("contain", "My First Website");
-      cy.get(".description").should("contain", 
-      "This was my first project.");
+      cy.get(".header").should("contain", "My First Project");
+      cy.get(".description").should(
+        "contain",
+        "My first major project included creating a website where you can store contact information"
+      );
     });
   });
+
   it("displays the second project", () => {
     cy.get("#project-2").within(() => {
       cy.get(".image").should("exist");
       cy.get(".header").should("contain", "FizzBuzz");
-      cy.get(".description").should(
-        "contain",
-        "Making ruby backend application is fun."
-      );
+      cy.get(".description").should("contain","One of the first things we did with Ruby was to create the FizzBuzz game");
     });
   });
-  it("displays the second project", () => {
+
+  it("displays the third project", () => {
     cy.get("#project-3").within(() => {
       cy.get(".image").should("exist");
-      cy.get(".header").should("contain", "Mobile UX");
+      cy.get(".header").should("contain", "Future Projects");
       cy.get(".description").should(
         "contain",
-        "I like to design for the mobile platform."
+        "Right now I am still learning the capabilities of the different languages that I am learning, but I am looking foward to what I can do with them once I am done."
       );
     });
   });
