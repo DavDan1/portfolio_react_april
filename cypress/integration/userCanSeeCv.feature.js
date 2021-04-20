2b12cb2deda6b9ef00c16964441e028532b3cb85
DavDan1 <57390886+DavDan1@users.noreply.github.com>
2021-04-20 08:13:54 +0200

    Update userCanSeeCv.feature.js

--- cypress/integration/userCanSeeCv.feature.js
+++ cypress/integration/userCanSeeCv.feature.js
@@ -20,24 +20,6 @@ describe("User can see My Cv", () => {
     cy.get("#projects-header").should("not.exist");
   });
 
- //it("displays headers for cv sections", () => {
-   // cy.get("#info-header").should("contain.text", "Personal Info");
-   // cy.get("#education-header").should("contain.text", "Education");
-  //});
-
-  //it("displays personal info", () => {
-  //cy.get("#cv-address").should("contain.text", "Mässvägen 15");
-
-    //cy.get(".country").should("contain.text", "Sweden");
-    //cy.get(".city").should("contain.text", "Stockholm");
-    //cy.get(".phone").should("contain.text", "+46729420089");
-   // cy.get(".email").should("contain.text", "david-danielyan@hotmail.com");
-  //});
-
-  //it("displays education", () => {
-  //  cy.get("#bachelors").should("contain.text", "Bachelor`s degree");
-  //  cy.get("#frontend-developer").should("contain.text", "Frontend Developer");
-  // cy.get("#fullstack-developer").should("contain.text","Fullstack Developer");
-  //});
+ 
 });
-//});
+
